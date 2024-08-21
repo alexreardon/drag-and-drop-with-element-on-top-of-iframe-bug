356d3748e85f3879b2010ae5a4f3168d1c43a296
Alex Reardon <alexreardon@gmail.com>
2024-08-21 19:28:06 +1000

    adding fix

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import {
   draggable,
   dropTargetForElements,
+  monitorForElements,
 } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 import { dropTargetForExternal } from '@atlaskit/pragmatic-drag-and-drop/external/adapter';
 import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
@@ -120,21 +121,38 @@ function Draggable() {
 }
 
 function Parent() {
-  const [iframeOnSameOrigin, setIframeOnSameOrigin] = useState<boolean>(true);
+  const [isIframeOnSameOrigin, setIsIframeOnSameOrigin] = useState<boolean>(true);
+  const [applyFix, setApplyFix] = useState<boolean>(false);
   const [isOnTop, setIsOnTop] = useState<boolean>(true);
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [isOver, setIsOver] = useState<boolean>(false);
+
+  useEffect(() => {
+    return monitorForElements({
+      onGenerateDragPreview() {
+        setIsDragging(true);
+      },
+      onDrop() {
+        setIsDragging(false);
+      }
+    })
+  }, []);
 
   return (
     <>
       <div className="relative flex flex-row gap-20">
         <iframe
-          src={iframeOnSameOrigin ? window.location.href : 'https://atlassian.design'}
+          ref={iframeRef}
+          src={isIframeOnSameOrigin ? window.location.href : 'https://atlassian.design'}
+          className={(isOver || isDragging) && applyFix ? 'pointer-events-none grayscale' : ''}
           width={600}
           height={600}
         />
         <div
-          className={`bg-slate-200 p-2 rounded border-2 border-black flex flex-col gap-2 w-80 h-80 ${
-            isOnTop ? 'absolute shadow-lg left-60 top-20' : ''
-          }`}
+          onMouseEnter={() => setIsOver(true)} onMouseLeave={() => setIsOver(false)}
+          className={`bg-slate-200 p-2 rounded border-2 border-black flex flex-col gap-2 w-80 h-80 ${isOnTop ? 'absolute shadow-lg left-60 top-20' : ''
+            }`}
         >
           <span>
             <code>position:absolute</code> element on top
@@ -144,14 +162,18 @@ function Parent() {
           <label className="flex flex-row gap-2">
             <input
               type="checkbox"
-              checked={iframeOnSameOrigin}
-              onChange={() => setIframeOnSameOrigin(!iframeOnSameOrigin)}
+              checked={isIframeOnSameOrigin}
+              onChange={() => setIsIframeOnSameOrigin(!isIframeOnSameOrigin)}
             />
             Place <code>iframe</code> on same origin?
           </label>
           <label className="flex flex-row gap-2">
             <input type="checkbox" checked={isOnTop} onChange={() => setIsOnTop(!isOnTop)} />
             Place this panel on top?
+          </label>
+          <label className="flex flex-row gap-2">
+            <input type="checkbox" checked={applyFix} onChange={() => setApplyFix(!applyFix)} />
+            Apply <code>pointer-events: none</code> fix?
           </label>
         </div>
       </div>
